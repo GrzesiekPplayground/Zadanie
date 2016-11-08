f329a52a6e2c0d0294997f2e3466cb3836224410
GrzesiekP <grzechu435@gmail.com>
2016-11-08 01:55:23 +0100

    Backend3: Works from hardcoded DropDownList

--- Zadanie/Controllers/HomeController.cs
+++ Zadanie/Controllers/HomeController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.Web.Mvc;
+using Zadanie.Models;
 
 namespace Zadanie.Controllers
 {
     public class HomeController : Controller
     {
+        private string _rootPath
+        {
+            get
+            {
+                return Server.MapPath("~");
+            }
+        }
+
+        private string _tableAdress
+        {
+            get
+            {
+                return "[" + _rootPath + "Data\\Earthquakes1970" + "]";
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -15,31 +32,45 @@ namespace Zadanie.Controllers
 
         public ActionResult Backend3()
         {
-            var querry = "SELECT LEFT(YYYYMMDD, 4) AS Year, Count(*) FROM d:\\data\\Earthquakes1970 GROUP BY Year";
-            var rows = myData(querry);
-            ViewBag.rows = myData(querry);
+            var year = "1970;";
+            var query = string.Format("SELECT LEFT(YYYYMMDD, 4) AS Year, Count(*) FROM {1} WHERE (YYYYMMDD LIKE '{0}%') GROUP BY Year", year, _tableAdress);
+            var rows = myData(query);
+            ViewBag.rows = myData(query);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Backend3(Earthquake eq)
+        {
+            var year = eq.selectedYear;
+            var query = string.Format("SELECT LEFT(YYYYMMDD, 4) AS Year, Count(*) FROM {1} WHERE (YYYYMMDD LIKE '{0}%') GROUP BY Year", year, _tableAdress);
+            var rows = myData(query);
+            ViewBag.rows = myData(query);
 
             return View();
         }
 
         public ActionResult Backend4()
         {
-            var querry = "SELECT * FROM d:\\data\\Earthquakes1970 WHERE (Num_Deaths > 1000) ORDER BY Num_Deaths";
-            var rows = myData(querry);
+            var query = string.Format("SELECT * FROM {0} WHERE (Num_Deaths > 1000) ORDER BY Num_Deaths", _tableAdress);
+            var rows = myData(query);
 
             ViewBag.rows = rows;
 
             return View();
         }
 
-        private List<DataRow> myData(string querry)
+        private List<DataRow> myData(string query)
         {
-            OleDbConnection conn = new OleDbConnection("Provider=VFPOLEDB.1;Data Source=c:\\Data\\;Extended Properties=dBASE IV;User ID=;Password=;");
+            var connectionString = string.Format("Provider=VFPOLEDB.1;Data Source={0}\\Data\\;Extended Properties=dBASE IV;User ID=;Password=;", _rootPath);
+            OleDbConnection conn = new OleDbConnection(connectionString);
             conn.Open();
 
-            OleDbCommand cmd = new OleDbCommand(querry, conn);
+            OleDbCommand cmd = new OleDbCommand(query, conn);
 
             DataTable dt = new DataTable();
+
             dt.Load(cmd.ExecuteReader());
 
             conn.Close();
---
+++ Zadanie/Models/Earthquake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Web.Mvc;
+
+namespace Zadanie.Models
+{
+    public class Earthquake
+    {
+
+        public string year { get; set; }
+        public string selectedYear { get; set; }
+
+        public static IEnumerable<SelectListItem> GetYearsSelectItems()
+        {
+            yield return new SelectListItem { Text = "1970", Value = "1970" };
+            yield return new SelectListItem { Text = "1980", Value = "1980" };
+        }
+
+
+    }
+}
